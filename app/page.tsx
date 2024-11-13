da841a0f7b189e69113a5a1287b11e4eead20206
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-13 18:35:08 +0000

    checkbox imported

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 import { useState } from 'react'
 import { Button } from '../components/ui/button'
 import { Input } from '../components/ui/input'
+import { Checkbox } from '../components/ui/checkbox'
 
 
 export default function Home() {
