06bb547826ae3b901e6dd06a1670a2e7b077db7f
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-13 18:34:35 +0000

    The inpurt component is imported

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import { useState } from 'react'
 import { Button } from '../components/ui/button'
+import { Input } from '../components/ui/input'
 
 
 export default function Home() {
