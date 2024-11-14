9640cd4a8378944d318a53ae2c2298cda87fafdb
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-14 13:26:24 +0000

    Already created the updateTodo setter

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,18 @@ type TodoStore = {
 
 const useTodoStore = create<TodoStore>((set) => ({
   todos: [],
-  
+
   addTodo: (text) => set((state) => ({
-    todo: [...state.todos, { id: Date.now(), text, completed: false }]
+    todos: [...state.todos, { id: Date.now(), text, completed: false }]
   }))
-  
+
+  toggleTodo: (id) => set((state) => ({
+    todos: state.todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed })
+  }))
+
+  updateTodo: (id, text) => set((state) => ({
+    todos: state.todos.map((todo) => todo.id === id ? { ...todo, text })
+  }))
 }))
 
 
